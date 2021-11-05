04a259273bf498bfb91a8d9346d62c08a06df06a
Pedro Santos <espedrosantos@hotmail.com>
2021-11-05 20:44:09 -0300

    :rocket: Feature: starting catalog microservice implementation

---
+++ src/Services/Catalog/Catalog.API/Entities/Repositories/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+
+        Task CreateProduct(Product product);
+
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
