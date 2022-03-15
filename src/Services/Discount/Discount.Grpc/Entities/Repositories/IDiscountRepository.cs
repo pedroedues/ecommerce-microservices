8a423e564065fcd353916bc4f57e6d13f68573ae
Pedro Santos <espedrosantos@gmail.com>
2022-03-14 21:14:07 -0300

    :rocket: feature: Discount.Grpc project initialized

---
+++ src/Services/Discount/Discount.Grpc/Entities/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Discount.Grpc.Entities.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+
+        Task<bool> UpdateDiscount(Coupon coupon);
+
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
