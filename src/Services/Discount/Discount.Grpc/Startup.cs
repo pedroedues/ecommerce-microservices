78a50e0f25e46f8de16c66ea59b865d5b6b7e817
Pedro Santos <espedrosantos@gmail.com>
2022-03-17 22:30:00 -0300

    :rocket: feature: Implementing Discount.Grpc

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -30,7 +30,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
