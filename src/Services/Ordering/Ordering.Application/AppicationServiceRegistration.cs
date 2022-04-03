69e10cb341f978187e2787e3c3ba9cf92cc2d203
Pedro Santos <espedrosantos@gmail.com>
2022-04-03 07:54:01 -0300

    feature: Finalizing application layer implementing dependency injections

---
+++ src/Services/Ordering/Ordering.Application/AppicationServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+using MediatR;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+using Ordering.Application.Behaviours;
+
+namespace Ordering.Application
+{
+    public static class AppicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
