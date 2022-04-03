55475bd7a2618af82bda6ae388c2f7ca34e93c2c
Pedro Santos <espedrosantos@gmail.com>
2022-04-03 08:28:17 -0300

    :rocket: feature: Implementing Order.API controller

--- src/Services/Ordering/Ordering.Application/Features/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -4,10 +4,16 @@ using Ordering.Application.Features.Commands.CheckoutOrder;
 
 namespace Ordering.Application.Features.Commands.UpdateOrder
 {
-    public class UpdateOrderCommand  : IRequest
+    public class UpdateOrderCommand : IRequest
     {
         public int Id { get; private set; }
 
         public CheckoutUpdateOrderViewModel Order { get; private set; }
+        
+        public UpdateOrderCommand(int id, CheckoutUpdateOrderViewModel order)
+        {
+            Id = id;
+            Order = order;
+        }
     }
 }
